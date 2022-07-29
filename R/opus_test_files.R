5b2331ff695463ba5f9958c9115e09a404f53cad
Philipp Baumann <baumann-philipp@proton.me>
2022-07-29 12:36:18 +0200

    add test data from {opusreader}

---
+++ R/opus_test_files.R
@@ -0,0 +1,11 @@
+#' @name opus_file
+#' @title Get location of a sample OPUS file
+#' @description Utility function that retrieves the location of the sample OPUS binary file on disc.
+#' @return a character vector storing the location of the sample OPUS file
+#' @export
+#' @examples
+#' fn <- opus_file()
+#' fn
+opus_file <- function() {
+  system.file("extdata", "test_spectra.0", package = "opusreader")
+}
