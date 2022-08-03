5713f62a3b2019472745801ce188ee382821ca65
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-08-03 22:14:05 +0200

    adjust to new system

--- R/read_header.R
+++ R/read_header.R
@@ -4,10 +4,17 @@ read_header <- function(raw, nh = 504L) {
 
   # set start cursor in bytes
   # cursor <- 32L; in ONO, the start cursor is set to this
-  cursor <- 44L
+  cursor <- 32L
 
   # file size; n data
   nd <- length(raw)
+
+  offset_list <- list()
+  chuck_size_list <- list()
+  data_type_list <- list()
+  data_text_list <- list()
+  channel_type_list <- list()
+
 
   while (cursor > 0L) {
     i1 <- cursor
@@ -18,26 +25,30 @@ read_header <- function(raw, nh = 504L) {
       # position the connection and read from current offset
       seek(con, where = i1, origin = "start", rw = "read")
       # read the offset
-      offset <- readBin(con, what = "integer", n = 1L, size = 4L)
+      offset <- readBin(con, what = "integer", n = 1L, size = 4L, endian = "little")
 
       if (offset > 0) {
+        offset_list <- c(offset_list, offset)
 
         # read chunk size (4 bytes)
         seek(con, where = i1 - 4L, origin = "start", rw = "read")
         chunk_size <- readBin(con, what = "integer", n = 1L, size = 4L)
+        chuck_size_list <- c(chuck_size_list, chunk_size)
 
         # read the data type # => does not work yet
         i1 <- cursor - 8L
-        i2 <- i1 + 1L
+        i2 <- cursor + 1L
         seek(con, where = i1, origin = "start", rw = "read")
         # read as character
         type <- readBin(
           con,
-          what = "character",
+          what = "integer",
           n = 1L,
-          size = 1L,
-          endian = "little"
+          size = 1,
+          endian = "little",
+          signed = F
         )
+        data_type_list <- c(data_type_list, type)
 
         # read the channel type
         i1 <- cursor - 7L
@@ -45,11 +56,13 @@ read_header <- function(raw, nh = 504L) {
         seek(con, where = i1, origin = "start", rw = "read")
         channel <- readBin(
           con,
-          what = "character",
+          what = "integer",
           n = 1L,
-          size = 1L,
-          endian = "little"
+          size = 1,
+          endian = "little",
+          signed = F
         )
+        channel_type_list <- c(channel_type_list, channel)
 
         # read the text type
         i1 <- cursor - 6L
@@ -57,11 +70,13 @@ read_header <- function(raw, nh = 504L) {
         seek(con, where = i1, origin = "start", rw = "read")
         text <- readBin(
           con,
-          what = "character",
+          what = "integer",
           n = 1L,
-          size = 1L,
-          endian = "little"
+          size = 1,
+          endian = "little",
+          signed = F
         )
+        data_text_list <- c(data_text_list, text)
 
         next_offset <- offset + 4 * chunk_size
 
@@ -77,4 +92,16 @@ read_header <- function(raw, nh = 504L) {
       cursor <- -1L
     }
   }
+
+  result <- list(offset_list = offset_list,
+                 chuck_size_list = chuck_size_list,
+                 data_type_list = data_type_list,
+                 data_text_list = data_text_list,
+                 channel_type_list = channel_type_list)
+
+  return(result)
 }
+
+
+
+
