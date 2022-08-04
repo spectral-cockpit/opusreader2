cf66ece95ffbe32800488c0d462a23a90e274019
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-08-04 21:24:02 +0200

    something like that

--- inst/test.R
+++ inst/test.R
@@ -1,7 +1,7 @@
 raw <- read_opus_file("inst/extdata/test_data/629266_1TP A-1_C1.0")
 
-test1 <- read_header(raw)
+header_data <- read_header(raw)
 
+dataset_list <- lapply(header_data, create_dataset)
 
-
-
+dataset_list <- lapply(dataset_list, read_chunk)
