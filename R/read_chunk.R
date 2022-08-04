9294d3a1ae530d5cd7b21530cff99de2c950f719
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-08-04 20:53:01 +0200

    read_chunk methods

---
+++ R/read_chunk.R
@@ -0,0 +1,17 @@
+read_chunk <- function(con, ds) UseMethod("read_chunk", con, ds)
+
+read_chunk.text <- function(con, ds){
+
+
+}
+
+read_chunk.parameter <- function(con, ds){
+
+
+}
+
+read_chunk.data <- function(con, ds){
+
+
+
+}
