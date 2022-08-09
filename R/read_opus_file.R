0d3405c84d641aa759d88fda47b35046acaf3c11
Philipp Baumann <philipp.baumann@bfh.ch>
2022-08-09 16:11:50 +0200

    template reader impl

--- R/read_opus_file.R
+++ R/read_opus_file.R
@@ -1,4 +1,9 @@
 read_opus_file <- function(file) {
+  file_size <- file.size(file)
   # Get raw vector
-  raw <- readBin(file, "raw", n = file.size(file))
+  raw <- readBin(file, "raw", n = file_size)
+
+  header_data <- read_header(raw)
+  dataset_list <- lapply(header_data, create_dataset)
+  dataset_list <- lapply(dataset_list, read_chunk)
 }
