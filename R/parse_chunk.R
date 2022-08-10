46eee65c28e61d8d9b4e394ace56b5dbcf3bfbff
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-08-10 18:27:44 +0200

    add parsing steps

--- R/parse_chunk.R
+++ R/parse_chunk.R
@@ -1,30 +1,31 @@
 #' read chunk method
 #' @export
-parse_chunk <- function(ds, con) UseMethod("read_chunk")
+parse_chunk <- function(ds, con) UseMethod("parse_chunk")
 
 #' read chunk method for text
 #' @export
-parse_chunk.text <- function(ds, con){
+parse_chunk.text <- function(ds, con) {
 
+  text <- read_character(con, ds$offset, n = ds$chunck_size)
 
-  # ds$value <- value
-  # return(ds)
+  ds$text <- text
+  return(ds)
 }
 
 #' read chunk method for parameter
 #' @export
-parse_chunk.parameter <- function(ds, con){
-
+parse_chunk.parameter <- function(ds, con) {
   cursor <- ds$offset
   chunk_size <- ds$chunk_size
 
-  parameter_types <- c('int', 'float', 'str', 'str', 'str')
+  parameter_types <- c("int", "float", "str", "str", "str")
+
+  result_list <- list()
 
   repeat {
-
     parameter_name <- read_character(con, cursor, n = 3L)
 
-    if(parameter_name == "END"){
+    if (parameter_name == "END") {
       break
     }
 
@@ -35,17 +36,23 @@ parse_chunk.parameter <- function(ds, con){
     parameter_size <- read_unsigned_int(con, cursor + 6, n = 2L)
 
     cursor_value <- cursor + 8
-    n_value <- 2*parameter_size
-
+    n_value <- 2 * parameter_size
+browser()
     if (type_index == 0) {
-      parameter_value <- read_signed_int(con, cursor_value, n =  n_value)
+      parameter_value <- read_signed_int(con, cursor_value, n = n_value)
     } else if (type_index == 1) {
       parameter_value <- read_double(con, cursor_value, n = n_value)
-    } else if (type_index %in% c(2,3,4)) {
+    } else if (type_index %in% c(2, 3, 4)) {
       parameter_value <- read_character(con, cursor_value, n = n_value)
     }
 
+    repeat_list <- list(
+      parameter_name = parameter_name,
+      parameter_value = parameter_value,
+      parameter_type = parameter_type
+    )
 
+    result_list <- c(result_list, list(repeat_list))
 
 
     cursor <- cursor + 8 + 2 * parameter_size
@@ -53,21 +60,17 @@ parse_chunk.parameter <- function(ds, con){
     if (cursor >= chunk_size) {
       break
     }
-
   }
 
-  # browser()
-
-  # ds$value <- value
-  # return(ds)
+  ds$parameter <- result_list
+  return(ds)
 }
 
 #' read chunk method for data
 #' @export
-parse_chunk.data <- function(ds, con){
+parse_chunk.data <- function(ds, con) {
+  data <- read_double(con, ds$offset, n = ds$chunk_size)
 
-  channel_type <- read_double(con, ds$offset, n = ds$chunk_size)
-
-  ds$value <- value
+  ds$data <- data
   return(ds)
 }
