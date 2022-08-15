778f4b0f826ff57c667bc259c98f6f4decbfac9e
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-08-15 23:18:51 +0200

    rename the output list inclueding spec_no_atm_comp

---
+++ R/name_output_list.R
@@ -0,0 +1,23 @@
+name_output_list <- function(ds_list){
+
+  block_names <- unlist(lapply(ds_list, function(x) x$block_type_name))
+  block_names <- add_spec_no_atm_comp(block_names)
+  names(ds_list) <- block_names
+
+  return(ds_list)
+}
+
+add_spec_no_atm_comp <- function(x){
+
+  i <- which(grepl("spec$",x))
+  j <- which(grepl("spec_data_param", x))
+
+  if(length(i) == 1){
+    return(x)
+  }
+
+  x[head(i,1)] <- "spec_no_atm_comp"
+  x[head(j,1)] <- "spec_no_atm_comp_data_param"
+
+  return(x)
+}
