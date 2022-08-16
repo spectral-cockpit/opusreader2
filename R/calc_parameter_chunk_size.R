4eafd4734f637f4c4b8a599a94988a0642bafc5a
Philipp Baumann <baumann-philipp@proton.me>
2022-08-16 15:41:43 +0200

    minor manual linting

--- R/calc_parameter_chunk_size.R
+++ R/calc_parameter_chunk_size.R
@@ -2,30 +2,23 @@
 calc_parameter_chunk_size <- function(ds) UseMethod("calc_parameter_chunk_size")
 
 #' @export
-calc_parameter_chunk_size.parameter <- function(ds){
-
+calc_parameter_chunk_size.parameter <- function(ds) {
   ds$chunk_size <- calc_chunk_size(ds)
-
-  return(ds)
-
-}
-
-#' @export
-calc_parameter_chunk_size.text <- function(ds){
-
-  ds$chunk_size <- calc_chunk_size(ds)
-
   return(ds)
 }
 
 #' @export
-calc_parameter_chunk_size.data <- function(ds){
+calc_parameter_chunk_size.text <- function(ds) {
+  ds$chunk_size <- calc_chunk_size(ds)
   return(ds)
 }
 
-calc_chunk_size <- function(ds){
+#' @export
+calc_parameter_chunk_size.data <- function(ds) {
+  return(ds)
+}
 
+calc_chunk_size <- function(ds) {
   chunk_size <- ds$next_offse - ds$offset
-
   return(chunk_size)
 }
