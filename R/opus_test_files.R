a8379435f624728937d54d0178eb1f5ddab5495d
Philipp Baumann <baumann-philipp@proton.me>
2022-08-17 11:54:12 +0200

    typo

--- R/opus_test_files.R
+++ R/opus_test_files.R
@@ -1,7 +1,7 @@
 #' @name opus_file
 #' @title Get location of a sample OPUS file
 #' @description Utility function that retrieves the location of the sample OPUS
-#' binary file on disc.
+#' binary file on disk.
 #' @return a character vector storing the location of the sample OPUS file
 #' @export
 #' @examples
