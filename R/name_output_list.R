5efbfd25bfb12ad524ebecc99a4ef95abe6d05fb
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-08-18 17:06:45 +0200

    rename lab process list

--- R/name_output_list.R
+++ R/name_output_list.R
@@ -1,6 +1,7 @@
 name_output_list <- function(ds_list) {
   block_names <- unlist(lapply(ds_list, function(x) x$block_type_name))
   block_names <- add_spec_no_atm_comp(block_names)
+  block_names <- add_lab_process_name(block_names)
   names(ds_list) <- block_names
 
   return(ds_list)
@@ -16,6 +17,24 @@ add_spec_no_atm_comp <- function(x) {
 
   x[i[1]] <- "spec_no_atm_comp"
   x[j[1]] <- "spec_no_atm_comp_data_param"
+
+  return(x)
+}
+
+add_lab_process_name <- function(x){
+
+  i <- which(grepl("lab_and_", x))
+
+  z <- 1
+
+  if(length(i) > 1){
+    for (j in i) {
+
+      x[j] <- paste0("lab_and_process_param_", z)
+      z <- z+1
+
+    }
+  }
 
   return(x)
 }
