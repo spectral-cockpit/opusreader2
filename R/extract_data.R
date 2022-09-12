15966049e23bf2de87975c7360a5bd4cca022fa5
Thomas Knecht <thomasknecht@Thomass-MacBook-Air.local>
2022-09-12 20:30:37 +0200

    helpfer function to extract the needed list entries for the data

---
+++ R/extract_data.R
@@ -0,0 +1,11 @@
+extract_data <- function(dataset_list, filter_vector){
+  dataset_list <- dataset_list[
+    lapply(
+      dataset_list,
+      function(x) x$block_type_name
+    ) %in%
+      c(filter_vector)
+  ]
+
+  return(dataset_list)
+}
