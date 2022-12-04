2a754804072195bd6d3cd4dc98a137d6dafb0c76
Philipp Baumann <baumann-philipp@proton.me>
2022-12-04 14:23:51 +0100

    add linter und styler configuration

--- R/seek_opus.R
+++ R/seek_opus.R
@@ -1,6 +1,6 @@
 #' reposition cursor for open read connection
 #' @param con connection to raw vector
 #' @param cursor cursor position within the connection
-seek_opus <- function(con, cursor) {
+seek_opus = function(con, cursor) {
   seek(con, where = cursor, origin = "start", rw = "read")
 }
