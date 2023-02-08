00d6ae5ea4f4a683eaa1bade6cd33b873dc833eb
Philipp Baumann <baumann-philipp@proton.me>
2023-02-08 18:05:34 +0100

    fix R CMD check note

--- R/check_functions.R
+++ R/check_functions.R
@@ -13,11 +13,11 @@ check_character <- function(param) {
 }
 
 check_future <- function() {
-  if (!require("future")) {
+  if (!requireNamespace("future")) {
     stop('To use the parallel option, install the package: future, first.\n
           Use `install.package("future")`')
   }
-  if (!require("future.apply")) {
+  if (!requireNamespace("future.apply")) {
     stop(
       "To use the parallel option, install the package: future.apply,",
       'first.\n Use `install.package("future.apply")`'
