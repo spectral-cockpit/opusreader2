a39afa4106bc3c7a8ad25a1b03d6d96c5a129c0c
Philipp Baumann <baumann-philipp@proton.me>
2023-02-15 05:59:42 +0100

    prepend class also for parser

--- R/parse_opus.R
+++ R/parse_opus.R
@@ -180,7 +180,7 @@ parse_opus <- function(raw, data_only) {
 
   on.exit(close(con))
 
-  class(dataset_list) <- "list2_opusreader2"
+  class(dataset_list) <- c("list2_opusreader2", class(dataset_list))
 
   return(dataset_list)
 }
