c7878593a3f8138da3bab36cb6bba6473ce9afd2
ThomasKnecht <t.knecht@hotmail.com>
2023-11-11 17:10:07 +0100

    add initial tests

---
+++ tests/testthat/test-parse_header.R
@@ -0,0 +1,32 @@
+test_that("header is correctly parsed", {
+  raw <- read_opus_raw("../../inst/extdata/test_data/test_spectra.0")
+
+  con <- rawConnection(raw)
+
+  raw_size <- length(raw)
+
+  header_data <- parse_header(raw_size, con)
+
+  expect_length(header_data, 20)
+
+  header_data_unlist <- unlist(header_data)
+
+  expect_true(is.integer(header_data_unlist))
+
+  expect_length(header_data_unlist, 140)
+
+  block_type_names <- unique(unlist(lapply(header_data, names)))
+
+  expected_block_type_names <- c(
+    "block_type", "channel_type",
+    "text_type", "additional_type",
+    "offset", "next_offset",
+    "chunk_size"
+  )
+
+  expect_true(all(block_type_names %in% expected_block_type_names))
+
+})
+
+
+
