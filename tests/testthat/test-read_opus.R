0d73b4ff038c2b468809c6d0eee79a45509cd268
Philipp Baumann <baumann-philipp@protonmail.com>
2024-02-29 14:56:12 +0100

    please lintr

--- tests/testthat/test-read_opus.R
+++ tests/testthat/test-read_opus.R
@@ -3,7 +3,8 @@ test_that("all test files are parsed without warnings and errors", {
   expect_no_warning(read_opus(dsn = "../../inst/extdata/test_data"))
 })
 
-test_that("all test files are parsed without warnings and errors when `data_only = TRUE`", {
+test_that("all test files are parsed without warnings and errors when
+          `data_only = TRUE`", {
   expect_no_error(
     read_opus(dsn = "../../inst/extdata/test_data", data_only = TRUE)
   )
