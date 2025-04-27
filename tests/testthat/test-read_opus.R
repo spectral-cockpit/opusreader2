7ac6acec48d3b67283f3fcf12508cb53326144c9
Philipp Baumann <baumann-philipp@protonmail.com>
2025-04-27 11:56:27 +0200

    `opus_test_file()` and `opus_test_dsn()`

--- tests/testthat/test-read_opus.R
+++ tests/testthat/test-read_opus.R
@@ -1,14 +1,14 @@
 test_that("all test files are parsed without warnings and errors", {
-  expect_no_error(read_opus(dsn = "../../inst/extdata/test_data"))
-  expect_no_warning(read_opus(dsn = "../../inst/extdata/test_data"))
+  expect_no_error(read_opus(dsn = opus_test_dsn()))
+  expect_no_warning(read_opus(dsn = opus_test_dsn()))
 })
 
 test_that("all test files are parsed without warnings and errors when
           `data_only = TRUE`", {
   expect_no_error(
-    read_opus(dsn = "../../inst/extdata/test_data", data_only = TRUE)
+    read_opus(dsn = opus_test_dsn(), data_only = TRUE)
   )
   expect_no_warning(
-    read_opus(dsn = "../../inst/extdata/test_data", data_only = TRUE)
+    read_opus(dsn = opus_test_dsn(), data_only = TRUE)
   )
 })
