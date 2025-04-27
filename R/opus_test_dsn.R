5b4e3feec82cba1d7d99a0c2b8c39b44d3243ca1
Philipp Baumann <baumann-philipp@protonmail.com>
2025-04-27 12:00:30 +0200

    rename

--- R/opus_test_dsn.R
+++ R/opus_test_dsn.R
@@ -19,7 +19,7 @@ opus_test_file <- function() {
 #' @return a character vector with the paths OPUS files included in the package
 #' @export
 #' @examples
-#' (fn <- opus_test_files())
+#' (dsn <- opus_test_dsn))
 opus_test_dsn <- function() {
   system.file("extdata", "test_data", package = "opusreader2")
 }
