33e0be4a122d2be56d86b3f23627a80ef472b636
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-10-20 20:48:20 +0200

    fix(check): use x as data

--- R/print.R
+++ R/print.R
@@ -3,7 +3,7 @@ print.list_opusreader2 <- function(x, ...) {
   len <- length(x)
   nm <- if (length(x) != 1) "spectra" else "spectrum"
   cat(paste0("OPUS collection with ", bf(), len, " ", nm, nf(), "\n"))
-  utc_times <- unlist(lapply(data, function(x) x$basic_metadata$utc_datetime_posixct))
+  utc_times <- unlist(lapply(x, function(x) x$basic_metadata$utc_datetime_posixct))
   utc_range <- as.POSIXct(range(utc_times), origin = "1970-01-01", tz = "UTC")
   cat(paste0(bf(), "Measurement metadata:", nf(), "\n")) 
   cat(paste0("  * date-time range (UTC): ", "[", format(utc_range[1]), " , ", format(utc_range[2]), "]", "\n"))
