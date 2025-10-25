d0c58f1ec31f95cdfb0767fcaac491530a277801
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-10-25 15:49:37 +0200

    fix performance benchmarking scripts

--- inst/scripts/bench_read_opus.R
+++ inst/scripts/bench_read_opus.R
@@ -1,5 +1,8 @@
 library("opusreader2")
 
 file <- opus_test_file()
-data <- read_opus(dsn = file)
-cat("=== Read 1 OPUS file ===\n")
+files_1000 <- rep(file, 1000L)
+
+data <- read_opus(dsn = files_1000)
+
+cat("=== Read 1000 OPUS file ===\n")
--- inst/scripts/bench_read_opus_parallel.R
+++ inst/scripts/bench_read_opus_parallel.R
@@ -6,6 +6,8 @@ daemons(n = 8L)
 file <- opus_test_file()
 files_1000 <- rep(file, 1000L)
 
-data <- read_opus(dsn = file, parallel = TRUE)
+data <- read_opus(dsn = files_1000, parallel = TRUE)
+
+print(data)
 
 cat("=== Read 1000 OPUS files ===\n")
