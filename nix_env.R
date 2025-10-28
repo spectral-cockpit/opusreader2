647222748a4576c7fdcbf2100d17ca21c9203e34
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-10-28 07:17:48 +0100

    fix(ci): add all required packages

--- nix_env.R
+++ nix_env.R
@@ -1,8 +1,10 @@
 library("rix")
 
+r_pkgs <- c("rcmdcheck", "knitr", "rmarkdown", "testthat", "progressr", "mirai")
+
 rix(
   r_ver = "latest-upstream",
-  r_pkgs = c("rcmdcheck"),
+  r_pkgs = r_pkgs,
   ide = "none",
   project_path = ".",
   overwrite = TRUE
