1c128cfc8f5c4510bb7984823b2390f40c4c77e3
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-10-28 07:32:01 +0100

    chore: use mirai from github

--- nix_env.R
+++ nix_env.R
@@ -1,10 +1,17 @@
 library("rix")
 
-r_pkgs <- c("rcmdcheck", "knitr", "rmarkdown", "testthat", "progressr", "mirai")
+cran_pkgs <- c("rcmdcheck", "knitr", "rmarkdown", "testthat", "progressr")
+
+git_pkgs <- list(
+  package_name = "mirai",
+  repo_url = "https://github.com/r-lib/mirai/",
+  commit = "6895fea0853515f01deb67d3fd3890fcaf1105b0"
+)
 
 rix(
   r_ver = "latest-upstream",
-  r_pkgs = r_pkgs,
+  r_pkgs = cran_pkgs,
+  git_pkgs = git_pkgs,
   ide = "none",
   project_path = ".",
   overwrite = TRUE
