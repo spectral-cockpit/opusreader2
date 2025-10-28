26aea8125f5f7a7eea0a7843c99e2af5c5ceed52
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-10-28 14:30:55 +0100

    try fix warning

--- nix_env.R
+++ nix_env.R
@@ -2,24 +2,10 @@ library("rix")
 
 cran_pkgs <- c("rcmdcheck", "knitr", "rmarkdown", "testthat", "progressr")
 
-# git_pkgs <- list(
-#   list(
-#     package_name = "mirai",
-#     repo_url = "https://github.com/r-lib/mirai/",
-#     commit = "6895fea0853515f01deb67d3fd3890fcaf1105b0"
-#   ),
-#   list(
-#     package_name = "nanonext",
-#     repo_url = "https://github.com/r-lib/nanonext",
-#     commit = "ad23cd52ffcf7f6d285a525181a06aedf361541e"
-#   )
-# )
-
 rix(
   r_ver = "latest-upstream",
   r_pkgs = cran_pkgs,
-  # system_pkgs = c("cmake", "mbedtls"),
-  # git_pkgs = git_pkgs,
+  system_pkgs = "html-tidy",
   tex_pkgs = c("amsmath", "inconsolata"),
   ide = "none",
   project_path = ".",
