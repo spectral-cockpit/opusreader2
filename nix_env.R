a68c2d2fc6a2b96320269aaef835aa42af8d71b6
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-10-30 18:57:09 +0100

    fix(ci): add qpdf

--- nix_env.R
+++ nix_env.R
@@ -5,7 +5,7 @@ cran_pkgs <- c("rcmdcheck", "knitr", "rmarkdown", "testthat", "progressr")
 rix(
   r_ver = "latest-upstream",
   r_pkgs = cran_pkgs,
-  system_pkgs = "html-tidy",
+  system_pkgs = c("html-tidy", "qpdf"),
   tex_pkgs = c("amsmath", "inconsolata"),
   ide = "none",
   project_path = ".",
