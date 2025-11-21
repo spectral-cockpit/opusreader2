03aa178bf77dada9ecc83cbaa61312659690b43b
Philipp Baumann <baumann-philipp@protonmail.ch>
2025-11-21 14:23:00 +0100

    feat: print data blocks

--- R/print.R
+++ R/print.R
@@ -3,7 +3,9 @@ print.list_opusreader2 <- function(x, ...) {
   len <- length(x)
   nm <- if (length(x) != 1) "spectra" else "spectrum"
   cat(paste0("OPUS collection with ", bf(), len, " ", nm, nf(), "\n"))
-  utc_times <- unlist(lapply(x, function(x) x$basic_metadata$utc_datetime_posixct))
+  utc_times <- unlist(
+    lapply(x, function(x) x$basic_metadata$utc_datetime_posixct)
+  )
   utc_range <- as.POSIXct(range(utc_times), origin = "1970-01-01", tz = "UTC")
   cat(paste0(bf(), "Measurement metadata:", nf(), "\n"))
   cat(paste0(
@@ -15,6 +17,30 @@ print.list_opusreader2 <- function(x, ...) {
     "]",
     "\n"
   ))
+
+  data_names <- unlist(lapply(data, function(x) get_class_names(x, "data")))
+  data_tab <- table(data_names)
+  param_names <- unlist(lapply(data, function(x) {
+    get_class_names(x, "parameter")
+  }))
+  param_tab <- table(param_names)
+  text_names <- unlist(lapply(data, function(x) get_class_names(x, "text")))
+  text_tab <- table(text_names)
+
+  cat(
+    bf(),
+    "* data blocks:\n",
+    nf(),
+    paste0(
+      "  - ",
+      names(data_tab),
+      " (",
+      as.character(unname(data_tab)),
+      ")",
+      "\n"
+    )
+  )
+
   invisible(x)
 }
 
@@ -25,3 +51,13 @@ bf <- function() if (get_ostype() == "unix") "\x1B[1m" else ""
 nf <- function() if (get_ostype() == "unix") "\x1B[22m" else ""
 
 get_ostype <- function() .Platform$OS.type
+
+filter_class <- function(x, class = c("data", "parameter", "text")) {
+  cls <- match.arg(class)
+  x[vapply(x, function(x) class(x) == cls, logical(1))]
+}
+
+get_class_names <- function(x, class = c("data", "parameter", "text")) {
+  x_class <- filter_class(x, class = class)
+  names(x_class)
+}
